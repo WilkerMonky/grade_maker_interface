57bc9cd5502be27948ba22ba31306e100fad0120
wilker <weslleywilker10@gmail.com>
2024-09-28 17:19:01 -0300

    remocao-primereact

--- grade_maker/src/App.jsx
+++ grade_maker/src/App.jsx
@@ -1,9 +1,5 @@
 import { useState } from 'react'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-// Importa o tema do PrimeReact (você pode escolher outros temas)
-import 'primereact/resources/themes/soho-dark/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
 import './App.css'
 import AppRouter from './Routers'
 
